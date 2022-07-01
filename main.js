c2f43b19efd32a44adcaf4bfad17452d2e3cc7a3
Sviatoslove <svyatpopov86@gmail.com>
2022-07-02 01:42:16 +0300

    anime

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ function update () {
 };
 
 setTimeout(update, 5000);
+
+var atletTitle = document.querySelector('.atlet__title');
+
+var stepLeft = 30;
+var stepRight = 170;
+
+function animeLogoPlus() {
+  if(stepLeft >= 30 && stepLeft <= 170){
+    stepLeft += 1;
+    atletTitle.style.left = stepLeft + 'px';
+  }else if(stepRight <= 170 && stepRight >= 30){
+    stepRight -= 1;
+    atletTitle.style.left = stepRight + 'px';
+  }else {
+  stepLeft = 30;
+  stepRight = 170;
+  };
+};
+setInterval(animeLogoPlus, 10);
