2e024669334fc004cfbefdb10f693c37933f79e9
Sviatoslove <svyatpopov86@gmail.com>
2022-08-05 01:48:19 +0300

    modelWindowBegun

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-function update () {
-  alert('Кристина, ты меня игнорируешь??? В чём причина?');
-};
+// var modal = document.getElementById('wrapper-modal');
+// var overlay = document.getElementById('overlay');
+// var btnClose = document.getElementById('btn-close');
 
-setTimeout(update, 5000);
+// function closeModel() {
+//   modal.classList.remove('active');
+// };
+
+// overlay.addEventListener('click', closeModel);
+// // btnClose.addEventListener('click', closeModel);
+
+// function update () {
+//   // alert('Кристина, ты меня игнорируешь??? В чём причина?');
+//   modal.classList.add('active');
+// };
+
+// setTimeout(update, 100);
 
 var atletTitle = document.querySelector('.atlet__title');
 
-var stepLeft = 30;
-var stepRight = 170;
+var stepLeft = -60;
+var stepRight = 80;
 
-function animeLogoPlus() {
-  if(stepLeft >= 30 && stepLeft <= 170){
-    stepLeft += 1;
-    atletTitle.style.left = stepLeft + 'px';
-  }else if(stepRight <= 170 && stepRight >= 30){
-    stepRight -= 1;
-    atletTitle.style.left = stepRight + 'px';
-  }else {
-  stepLeft = 30;
-  stepRight = 170;
-  };
+function animeLogoPlus(a, b, ms) {
+  setInterval(() => {
+    if(stepLeft >= a && stepLeft <= b) {
+      stepLeft += 1;
+      atletTitle.style.left = stepLeft + 'px';
+    }else if(stepRight <= b && stepRight >= a) {
+      stepRight -= 1;
+      atletTitle.style.left = stepRight + 'px';
+    }else {
+    stepLeft = a;
+    stepRight = b;
+    };
+  }, ms);
 };
-setInterval(animeLogoPlus, 10);
+
+animeLogoPlus(stepLeft, stepRight, 10);
